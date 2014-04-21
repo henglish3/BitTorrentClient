9fbaf381b3bed2204d8d7ae094c04235a43a362a
Harrison English <henglishk@gmail.com>
2014-04-20 22:25:43 -0400

    blah blah blah fucked
    blah blah blah fucked

--- client.py
+++ client.py
@@ -3,6 +3,10 @@
 import sys, os, hashlib, StringIO
 import bencode
 import socket
+import struct
+import binascii
+import time
+import util
 from random import choice
 from hashlib import sha1
 
@@ -23,11 +27,14 @@ print hashlib.sha1(bencode.bencode(info)).hexdigest()
 
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
-port = 42044                # 25159 - HE; Reserve a port for your service.
+port = 25159                # 25159-HE /42044-BL; Reserve a port for your service.
 
 s.connect((host, port))
 print "connected."
 #s.close()	 # Close the socket when done
+
+# seed = socket.gethostname() + str(time.time())
+# peer_id = util.sha1_hash(seed) 
 
 # Handshake
 peer_id = ""
@@ -35,12 +42,19 @@ while len(peer_id) != 12:
 	peer_id = peer_id + choice("1234567890")
 
 peer_id = "-" + "PY" + "0001" + "-" + peer_id
-protocol_id = "BitTorrent protocol"
-len_id = str(len(protocol_id))
-reserved = "00000000"
-handshake = len_id + protocol_id + reserved + info_hash + peer_id
-#s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-#s.connect((host, port))
+# protocol_id = "BitTorrent protocol"
+# len_id = str(len(protocol_id))
+reserved = "0000000000000000000000000000000000000000000000000000000000000000"
+# handshake = "B"+len_id + protocol_id + reserved + info_hash + peer_id
+pstr = "BitTorrent protocol"
+handshake = "B"+str(len(pstr))+"s8x20s20s"+ str(len(pstr))+ pstr + info_hash +peer_id
+
+# print len(info)
+assert len(info_hash) == 20
+assert len(peer_id) == 20
+assert len(handshake) == (49+len(pstr))
+
+
 print handshake
 s.send(handshake)
 data = s.recv(len(handshake))
@@ -49,4 +63,5 @@ s.close()
 #end handshake and begin asking for pieces
 
 
-	# return data                  
+
+               
